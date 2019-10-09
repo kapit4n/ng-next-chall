11e4e1a05dcf61f75b5356dfc972602819677fde
Luis Arce <luis.arce22@gmail.com>
2019-10-08 21:52:21 -0400

    init: subject list and view pages

--- ui/src/app/subjects/subjects.module.ts
+++ ui/src/app/subjects/subjects.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SubjectsRoutingModule } from './subjects-routing.module';
+import { ListComponent } from './list/list.component';
+import { ViewComponent } from './view/view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent, ViewComponent],
   imports: [
     CommonModule,
     SubjectsRoutingModule
