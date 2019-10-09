ca0321b74a89994bb9415b98733111a6c6be9d89
Luis Arce <luis.arce22@gmail.com>
2019-10-08 22:02:00 -0400

    init: subject list and view routes

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'subjects',
+  loadChildren: () => import('./subjects/subjects.module').then(mod => mod.SubjectsModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/subjects/subjects-routing.module.ts
+++ ui/src/app/subjects/subjects-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { ViewComponent } from './view/view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ListComponent
+},
+{
+  path: ':id',
+  component: ViewComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
