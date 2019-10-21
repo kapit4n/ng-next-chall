eb88c49a48194a64739d935b8959306776f6c274
Luis Arce <luis.arce22@gmail.com>
2019-10-20 22:35:44 -0400

    server: add subject description

--- api/main.go
+++ api/main.go
@@ -8,9 +8,10 @@ type Category struct {
 }
 
 type Subject struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	CategoryId int    `json:"catId"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	CategoryId  int    `json:"catId"`
 }
 
 func main() {
@@ -32,9 +33,9 @@ func main() {
 
 	router.GET("/subjects", func(c *gin.Context) {
 		subjects := []Subject{
-			Subject{ID: 1, Name: "Angular", CategoryId: 1},
-			Subject{ID: 2, Name: "React Js", CategoryId: 1},
-			Subject{ID: 3, Name: "Vue Js", CategoryId: 1}}
+			Subject{ID: 1, Name: "Angular", CategoryId: 1, Description: "AngularJS is a JavaScript-based open-source front-end web framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. ... In 2014, the original AngularJS team began working on the Angular web framework."},
+			Subject{ID: 2, Name: "React Js", CategoryId: 1, Description: "React is the ultimate library for front-end developers today. Simply put, you get better at development when you learn React, and many organizations view these skills as essential. React developers should be hungry to level-up or audit the skills essential to Facebook's prominent JavaScript Library"},
+			Subject{ID: 3, Name: "Vue Js", CategoryId: 1, Description: "Vue.js (pronounced /vjuː/, like view) is a library for building interactive web interfaces. The goal of Vue.js is to provide the benefits of reactive data binding and composable view components with an API that is as simple as possible. Vue.js itself is not a full-blown framework - it is focused on the view layer only."}}
 		c.JSON(200, subjects)
 	})
 
