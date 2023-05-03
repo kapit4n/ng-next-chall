4b30e6bf31949d7918eb7bdd9503a7f3e006f000
Luis Arce <luis.arce22@gmail.com>
2023-05-03 10:37:16 -0400

    Add cors to server

--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,7 @@ module github.com/kapit4n/ng-next-chall/api
 go 1.12
 
 require (
-	github.com/gin-gonic/gin v1.7.0
-	github.com/kr/pretty v0.3.0 // indirect
+	github.com/gin-contrib/cors v1.4.0
+	github.com/gin-gonic/gin v1.8.1
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 type Category struct {
 	ID   int    `json:"id"`
@@ -16,6 +21,16 @@ type Subject struct {
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"categories": "/categories",
@@ -25,17 +40,17 @@ func main() {
 
 	router.GET("/categories", func(c *gin.Context) {
 		categories := []Category{
-			Category{ID: 1, Name: "UI Frameworks or Programming language"},
-			Category{ID: 2, Name: "Backend framework and programming language"},
-			Category{ID: 3, Name: "Styling framework"}}
+			{ID: 1, Name: "UI Frameworks or Programming language"},
+			{ID: 2, Name: "Backend framework and programming language"},
+			{ID: 3, Name: "Styling framework"}}
 		c.JSON(200, categories)
 	})
 
 	router.GET("/subjects", func(c *gin.Context) {
 		subjects := []Subject{
-			Subject{ID: 1, Name: "Angular", CategoryId: 1, Description: "AngularJS is a JavaScript-based open-source front-end web framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. ... In 2014, the original AngularJS team began working on the Angular web framework."},
-			Subject{ID: 2, Name: "React Js", CategoryId: 1, Description: "React is the ultimate library for front-end developers today. Simply put, you get better at development when you learn React, and many organizations view these skills as essential. React developers should be hungry to level-up or audit the skills essential to Facebook's prominent JavaScript Library"},
-			Subject{ID: 3, Name: "Vue Js", CategoryId: 1, Description: "Vue.js (pronounced /vjuː/, like view) is a library for building interactive web interfaces. The goal of Vue.js is to provide the benefits of reactive data binding and composable view components with an API that is as simple as possible. Vue.js itself is not a full-blown framework - it is focused on the view layer only."}}
+			{ID: 1, Name: "Angular", CategoryId: 1, Description: "AngularJS is a JavaScript-based open-source front-end web framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. ... In 2014, the original AngularJS team began working on the Angular web framework."},
+			{ID: 2, Name: "React Js", CategoryId: 1, Description: "React is the ultimate library for front-end developers today. Simply put, you get better at development when you learn React, and many organizations view these skills as essential. React developers should be hungry to level-up or audit the skills essential to Facebook's prominent JavaScript Library"},
+			{ID: 3, Name: "Vue Js", CategoryId: 1, Description: "Vue.js (pronounced /vjuː/, like view) is a library for building interactive web interfaces. The goal of Vue.js is to provide the benefits of reactive data binding and composable view components with an API that is as simple as possible. Vue.js itself is not a full-blown framework - it is focused on the view layer only."}}
 		c.JSON(200, subjects)
 	})
 
