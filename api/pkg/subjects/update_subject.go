c9adc85f3256618984b94a915e3097d68358b926
Luis Arce <luis.arce22@gmail.com>
2023-05-09 20:53:26 -0400

    api: Update subject

---
+++ api/pkg/subjects/update_subject.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kapit4n/ng-next-chall/api/pkg/common/models"
+)
+
+type UpdateSubjectRequestBody struct {
+	Name string `json:"name"`
+}
+
+func (h handler) UpdateSubject(c *gin.Context) {
+	id := c.Param("id")
+	body := UpdateSubjectRequestBody{}
+
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var subject models.Subject
+
+	if result := h.DB.First(&subject, id); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	subject.Name = body.Name
+
+	db.DB.Save(&subject)
+
+	c.JSON(http.StatusOK, &subject)
+}
