56382b86b40c80336308db57826d7eaa6fe82695
Luis Arce <luis.arce22@gmail.com>
2023-05-09 21:00:06 -0400

    api: register crud endpoints to router group subjects

--- api/pkg/subjects/controller.go
+++ api/pkg/subjects/controller.go
@@ -18,4 +18,8 @@ func RegisterRouters(r *gin.Engine, db *gorm.DB) {
 	routes := r.Group("/subjects")
 
 	routes.POST("/", h.AddSubject)
+	routes.GET("/", h.GetSubjects)
+	routes.GET("/:id", h.GetSubject)
+	routes.PUT("/:id", h.UpdateSubject)
+	routes.DELETE("/:id", h.DeleteSubject)
 }
--- api/pkg/subjects/update_subject.go
+++ api/pkg/subjects/update_subject.go
@@ -1,4 +1,4 @@
-package models
+package subjects
 
 import (
 	"net/http"
